84a50ab87830047dd29d2cc4071e473095e6e984
Sancheet <sanchethoque@gmail.com>
2023-01-25 17:32:29 -0500

    Changed signin response to userData

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.get("/", (req,res)=> {
 
 app.post("/signin", (req,res)=>{
 	if (req.body.email === database.users[1].email && req.body.password === database.users[1].password){
-			res.json("success")
+			res.json(database.users[1]);
 	} else {
 		res.json("Something went wrong")
 	}

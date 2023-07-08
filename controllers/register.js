c7a94c2b7cd067e9b74fdc0d43a1f588dc9fd08f
Sancheet <sanchethoque@gmail.com>
2023-07-07 23:37:48 -0400

    Added validation on server side for registration handler

--- controllers/register.js
+++ controllers/register.js
@@ -1,53 +1,61 @@
 const handleRegister= async (req, res, db, bcrypt)=>{
 	const {username,password,email} = req.body
+	const validRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
-	// try {
-	// 	const user = await db("users")
-	// 	.returning('*')
-	// 	.insert(
-	// 			{	
-	// 				username: username,
-	// 				email: email,
-	// 				joined: new Date()
-	// 			}
+	if (email==="" || password==="" || username==="") {
 
-	// 	)
-	// 	res.json(user[0]);
-	// }
-	// catch(err) {
-	// 	res.status(400).json("Unable to join");
-	// };
-	try {
-		await db.transaction(async trx => {
-			console.log(password);
+		res.status(400).json("Field is empty")
 
-			const hash = bcrypt.hashSync(password)
+	} else if (/\s/.test(username) || /\s/.test(email) || /\s/.test(password)) {
 
-			const loginEmail = await trx("login")
-			.insert(
-				{
-					hash: hash,
-					email: email
 
-				}
-			)
-			.returning('email')
+		res.status(400).json("Field has spaces")
 
-			const user = await trx("users")
-			.returning('*')
-			.insert(
-				{	
-					username: username,
-					email: loginEmail[0].email,
-					joined: new Date()
-				}
-			)
-			res.json(user[0]);
 
-		});
-	} catch (err) {
-		res.status(400).json("Error storing information");
-		console.log(err);
+	} else if (!validRegex.test(String(email).toLowerCase())) {
+
+
+		res.status(400).json("Email is not formatted correctly")
+
+
+
+
+
+	} else {
+
+
+		try {
+			await db.transaction(async trx => {
+				console.log(password);
+
+				const hash = bcrypt.hashSync(password)
+
+				const loginEmail = await trx("login")
+				.insert(
+					{
+						hash: hash,
+						email: email
+
+					}
+				)
+				.returning('email')
+
+				const user = await trx("users")
+				.returning('*')
+				.insert(
+					{	
+						username: username,
+						email: loginEmail[0].email,
+						joined: new Date()
+					}
+				)
+				res.json(user[0]);
+
+			});
+		} catch (err) {
+			res.status(400).json("Error storing information");
+			console.log(err);
+		}
 	}
 }
 
